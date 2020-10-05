0d0293a5b0edd63c5c9b7b844359b46ca6e1c640
Mustafa Arifoglu <mustafarifoglu@gmail.com>
2020-10-05 15:06:37 +0100

    NotificationFragment initialized

--- app/src/main/java/com/darth/on_road_vehicle_breakdown_help/view/NotificationFragment.kt
+++ app/src/main/java/com/darth/on_road_vehicle_breakdown_help/view/NotificationFragment.kt
@@ -5,22 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.darth.on_road_vehicle_breakdown_help.R
-import com.darth.on_road_vehicle_breakdown_help.databinding.FragmentHomeBinding
+import android.widget.Toast
 import com.darth.on_road_vehicle_breakdown_help.databinding.FragmentNotificationBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class NotificationFragment : Fragment() {
 
     private var _binding : FragmentNotificationBinding? = null
     private val binding get() = _binding!!
+
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
 
     }
 
@@ -30,13 +33,30 @@ class NotificationFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         _binding = FragmentNotificationBinding.inflate(inflater, container, false)
-
-
-
-
         return binding.root
     }
 
+    private fun getUserInformation() {
+
+        db.collection("UserInformation").addSnapshotListener { value, error ->
+            if (error != null) {
+                Toast.makeText(requireContext(), error.localizedMessage, Toast.LENGTH_SHORT).show()
+            } else {
+                if (value != null) {
+                    if (!value.isEmpty) {
+
+                        val documents = value.documents
+
+                        for (document in documents) {
+                            val userNameAndSurname = document.get("nameAndSurname") as String
+                            // Set the user name to a variable, too fucking long way again...
+
+                        }
+                    }
+                }
+            }
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
