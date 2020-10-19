ea83977b71553d12c440a90dcef2cd1fd1031cfe
Mustafa Arifoglu <mustafarifoglu@gmail.com>
2020-10-19 08:21:55 +0100

    Backend template added.

---
+++ web/scripts/index.js
@@ -0,0 +1,9 @@
+// Setup materialize components
+document.addEventListener('DOMContentLoaded', function() {
+
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+})
