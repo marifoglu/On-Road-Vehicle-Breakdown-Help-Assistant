c4b3cb32ec6c99fae3a1402ff5c0adc14410a609
Mustafa Arifoglu <mustafarifoglu@gmail.com>
2020-11-10 09:11:21 +0000

    Unnecessary files deleted.

--- web/scripts/index.js
+++
@@ -1,9 +0,0 @@
-// Setup materialize components
-document.addEventListener('DOMContentLoaded', function() {
-
-    var modals = document.querySelectorAll('.modal');
-    M.Modal.init(modals);
-
-    var items = document.querySelectorAll('.collapsible');
-    M.Collapsible.init(items);
-})
